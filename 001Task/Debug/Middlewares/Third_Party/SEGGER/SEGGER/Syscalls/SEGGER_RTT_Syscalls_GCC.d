32e5242aac5ffdb87c6fb6d901aaf6ccb2c1c285
Embedded-Geek-DE <mraihi.imededdine@gmail.com>
2025-05-12 15:56:15 +0200

    first commit

---
+++ 001Task/Debug/Middlewares/Third_Party/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../Middlewares/Third_Party/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ C:\workspace\001Task\Middlewares\Third_Party\SEGGER\SEGGER\SEGGER_RTT.h \
+ C:\workspace\001Task\Middlewares\Third_Party\SEGGER\Config\SEGGER_RTT_Conf.h
+C:\workspace\001Task\Middlewares\Third_Party\SEGGER\SEGGER\SEGGER_RTT.h:
+C:\workspace\001Task\Middlewares\Third_Party\SEGGER\Config\SEGGER_RTT_Conf.h:
